e1213fdd781309df5df2ed1d9c85453020fe17f0
Zahran03 <zalfarizi502@gmail.com>
2025-01-25 22:05:57 +0700

    learn var and string basic

---
+++ JAVASCRIPT/varAndStringBasic.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+const botName = "gpt";
+const botLocation = "pamulang";
+const favoriteLanguage = "JavaScript";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact =
+  "The " + favoriteLanguage + "is popular language in wev development";
+console.log(codingFact);
+codingFact =
+  "kamu bisa menambah interaktifitas di website kamu menggunakan " +
+  favoriteLanguage;
+console.log(codingFact);
+codingFact = "Sangat menyenangkan belajar " + favoriteLanguage;
+console.log(codingFact);
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    "."
+);
